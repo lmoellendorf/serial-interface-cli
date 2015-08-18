f4f18e2c8e52c5a4c783e214f5dfb92ebec5358a
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-08-18 17:05:21 +0200

    Auto formatting according to GNU standards
    Not sure if this will be the last word on formatting.
    
    refs #2936

--- cli/main.cpp
+++ cli/main.cpp
@@ -42,105 +42,106 @@ using namespace std;
 
 enum state
 {
-    START_FRAME,
-    APPEND_FRAME,
+  START_FRAME,
+  APPEND_FRAME,
 };
 
 struct app_ctx
 {
-    int run = TRUE;
-    int status = START_FRAME;
-    struct sp_port *port = NULL;
-    struct sf_serialmac_ctx *mac_ctx;
-    size_t iBuffLen = 0;
-    size_t oBuffRemains = 0;
-    size_t oBuffLength = 0;
-    char iBuff[SF_SERIAL_INPUT_MAX_SIZE];
-    char oBuff[SF_SERIAL_INPUT_MAX_SIZE];
+  int run = TRUE;
+  int status = START_FRAME;
+  struct sp_port *port = NULL;
+  struct sf_serialmac_ctx *mac_ctx;
+  size_t iBuffLen = 0;
+  size_t oBuffRemains = 0;
+  size_t oBuffLength = 0;
+  char iBuff[SF_SERIAL_INPUT_MAX_SIZE];
+  char oBuff[SF_SERIAL_INPUT_MAX_SIZE];
 };
 
 static struct app_ctx ctx;
 
-void read_evt(const char *frameBuffer, size_t frameBufferLength);
-void bufferRx_evt(const char *frameBuffer, size_t frameBufferLength);
-void write_evt(size_t processed);
-void bufferTx_evt(int processed);
-void wait4userinput(void);
-void wait4halEvent(enum sp_event event,
-                   enum sf_serialmac_return (*sf_serialmac_halCb)(struct sf_serialmac_ctx *ctx));
+
+void read_evt ( const char *frameBuffer, size_t frameBufferLength );
+void bufferRx_evt ( const char *frameBuffer, size_t frameBufferLength );
+void write_evt ( size_t processed );
+void bufferTx_evt ( int processed );
+void wait4userinput ( void );
+void wait4halEvent ( enum sp_event event,
+                     enum sf_serialmac_return ( *sf_serialmac_halCb ) ( struct sf_serialmac_ctx *ctx ) );
 void wait4halTxEvent();
 void wait4halRxEvent();
 
-void read_evt(const char *frameBuffer, size_t frameBufferLength)
+void read_evt ( const char *frameBuffer, size_t frameBufferLength )
 {
   /**
    * FIXME: Be prepared: the MAC should also call this, when a frame has been
    * rejected.
    */
-    if (frameBuffer && frameBufferLength)
+  if ( frameBuffer && frameBufferLength )
     {
-        if ('\n' == frameBuffer[0])
+      if ( '\n' == frameBuffer[0] )
         {
-            ctx.run = FALSE;
+          ctx.run = FALSE;
         }
-        else
+      else
         {
-            printf(":%s:%zd\n", frameBuffer, frameBufferLength);
+          printf ( ":%s:%zd\n", frameBuffer, frameBufferLength );
         }
     }
 }
 
-void bufferRx_evt(const char *frameBuffer, size_t frameBufferLength)
+void bufferRx_evt ( const char *frameBuffer, size_t frameBufferLength )
 {
-    sf_serialmac_rx_frame((struct sf_serialmac_ctx *) ctx.mac_ctx, ctx.iBuff,
-                          sizeof(ctx.iBuff));
+  sf_serialmac_rx_frame ( ( struct sf_serialmac_ctx * ) ctx.mac_ctx, ctx.iBuff,
+                          sizeof ( ctx.iBuff ) );
 }
 
-void write_evt(size_t processed)
+void write_evt ( size_t processed )
 {
-    ctx.status = START_FRAME;
+  ctx.status = START_FRAME;
 }
 
-void bufferTx_evt(size_t processed)
+void bufferTx_evt ( size_t processed )
 {
-    ctx.oBuffRemains -= processed;
-    thread userInputEventLoop(wait4userinput);
-    userInputEventLoop.detach();
+  ctx.oBuffRemains -= processed;
+  thread userInputEventLoop ( wait4userinput );
+  userInputEventLoop.detach();
 }
 
-void wait4userinput(void)
+void wait4userinput ( void )
 {
-    enum sf_serialmac_return ret = SF_SERIALMAC_SUCCESS;
-    string line = "";
-    const size_t frmLength = 9;
+  enum sf_serialmac_return ret = SF_SERIALMAC_SUCCESS;
+  string line = "";
+  const size_t frmLength = 9;
 
-    if(!ctx.oBuffRemains)
+  if ( !ctx.oBuffRemains )
     {
-        printf("Input text:\n");
-        getline(cin, line);
-        if (line.length() > 0)
+      printf ( "Input text:\n" );
+      getline ( cin, line );
+      if ( line.length() > 0 )
         {
-            ctx.oBuffLength = ctx.oBuffRemains = line.length();
-            strncpy(ctx.oBuff,line.c_str(),sizeof ctx.oBuff);
+          ctx.oBuffLength = ctx.oBuffRemains = line.length();
+          strncpy ( ctx.oBuff, line.c_str(), sizeof ctx.oBuff );
         }
-        else
+      else
         {
-            /** Userinput was empty line -> STOP */
-            ctx.run = FALSE;
-            printf("Stop\n");
+          /** Userinput was empty line -> STOP */
+          ctx.run = FALSE;
+          printf ( "Stop\n" );
         }
     }
-    if(ctx.oBuffRemains)
+  if ( ctx.oBuffRemains )
     {
-        while((ret = sf_serialmac_tx_frame(ctx.mac_ctx, frmLength,
-                                           ctx.oBuff + (ctx.oBuffLength - ctx.oBuffRemains),
-                                           ctx.oBuffRemains)) != SF_SERIALMAC_SUCCESS)
+      while ( ( ret = sf_serialmac_tx_frame ( ctx.mac_ctx, frmLength,
+                                              ctx.oBuff + ( ctx.oBuffLength - ctx.oBuffRemains ),
+                                              ctx.oBuffRemains ) ) != SF_SERIALMAC_SUCCESS )
         {
-            printf("TX Error %i\nline: %s\nlength: %zd\n", ret,
-                   ctx.oBuff + (ctx.oBuffLength - ctx.oBuffRemains), ctx.oBuffRemains);
-            sleep(1);
+          printf ( "TX Error %i\nline: %s\nlength: %zd\n", ret,
+                   ctx.oBuff + ( ctx.oBuffLength - ctx.oBuffRemains ), ctx.oBuffRemains );
+          sleep ( 1 );
         }
-        //        switch (ctx.status)
+      //        switch (ctx.status)
 //        {
 //        case START_FRAME:
 //            if((ret = sf_serialmac_txFrameStart(ctx.mac_ctx,
@@ -169,12 +170,12 @@ void wait4userinput(void)
 
 void wait4halTxEvent()
 {
-    wait4halEvent(SP_EVENT_TX_READY, sf_serialmac_hal_tx_callback);
+  wait4halEvent ( SP_EVENT_TX_READY, sf_serialmac_hal_tx_callback );
 }
 
 void wait4halRxEvent()
 {
-    wait4halEvent(SP_EVENT_RX_READY, sf_serialmac_hal_rx_callback);
+  wait4halEvent ( SP_EVENT_RX_READY, sf_serialmac_hal_rx_callback );
 }
 /**
  * This is a method stub to react on libserialport events. The plan is to
@@ -184,47 +185,47 @@ void wait4halRxEvent()
  * <li>active polling</li>
  * </ul>
  */
-void wait4halEvent(enum sp_event event,
-                   enum sf_serialmac_return (*sf_serialmac_halCb)(struct sf_serialmac_ctx *ctx))
+void wait4halEvent ( enum sp_event event,
+                     enum sf_serialmac_return ( *sf_serialmac_halCb ) ( struct sf_serialmac_ctx *ctx ) )
 {
-    struct sp_event_set * portEventSet = NULL;
-    unsigned int portEventMask = event;
-    unsigned int timeout = 0;
+  struct sp_event_set *portEventSet = NULL;
+  unsigned int portEventMask = event;
+  unsigned int timeout = 0;
 
-    if (SP_OK <= sp_new_event_set(&portEventSet))
+  if ( SP_OK <= sp_new_event_set ( &portEventSet ) )
     {
 
-        if (SP_OK
-                <= sp_add_port_events(portEventSet, ctx.port,
-                                      (enum sp_event) portEventMask))
+      if ( SP_OK
+           <= sp_add_port_events ( portEventSet, ctx.port,
+                                   ( enum sp_event ) portEventMask ) )
         {
-            while (SP_OK <= sp_wait(portEventSet, timeout))
+          while ( SP_OK <= sp_wait ( portEventSet, timeout ) )
             {
 //                printf("masks: %X\nnumber of handles: %u\n",
 //                        (unsigned int) *(portEventSet->masks),
 //                        portEventSet->count);
-                sf_serialmac_halCb(ctx.mac_ctx);
-                sleep(1);
+              sf_serialmac_halCb ( ctx.mac_ctx );
+              sleep ( 1 );
             }
         }
-        sp_free_event_set(portEventSet);
+      sp_free_event_set ( portEventSet );
     }
-    return;
+  return;
 }
 
-int main(int argc, char **argv)
+int main ( int argc, char **argv )
 {
 
-    uint8_t mac_ctx[sf_serialmac_ctx_size()];
-    ctx.mac_ctx = (struct sf_serialmac_ctx*) mac_ctx;
+  uint8_t mac_ctx[sf_serialmac_ctx_size()];
+  ctx.mac_ctx = ( struct sf_serialmac_ctx * ) mac_ctx;
 
-    sp_return sp_ret = SP_OK;
+  sp_return sp_ret = SP_OK;
 //    struct sp_port **availablePorts = NULL;
-    const char portname[] = SF_SERIAL_PORT_NAME;
-    struct sp_port_config * savedPortConfig = NULL;
+  const char portname[] = SF_SERIAL_PORT_NAME;
+  struct sp_port_config *savedPortConfig = NULL;
 
-    //TODO: use sp_list_ports, look for valid port names and try to
-    //connect them or let the user choose.
+  //TODO: use sp_list_ports, look for valid port names and try to
+  //connect them or let the user choose.
 //    sp_ret = sp_list_ports(&availablePorts);
 //    if (SP_OK > sp_ret)
 //        return sp_ret;
@@ -239,99 +240,98 @@ int main(int argc, char **argv)
 //    if (SP_OK > sp_ret)
 //        return sp_ret;
 
-    sp_ret = sp_get_port_by_name(portname, &ctx.port);
-    if (SP_OK > sp_ret || NULL == ctx.port)
+  sp_ret = sp_get_port_by_name ( portname, &ctx.port );
+  if ( SP_OK > sp_ret || NULL == ctx.port )
     {
-        printf("Port \"%s\" could not be found!\n", portname);
-        return sp_ret;
+      printf ( "Port \"%s\" could not be found!\n", portname );
+      return sp_ret;
     }
 
-    sp_ret = sp_open(ctx.port, SP_MODE_READ_WRITE);
-    if (SP_OK > sp_ret)
+  sp_ret = sp_open ( ctx.port, SP_MODE_READ_WRITE );
+  if ( SP_OK > sp_ret )
     {
-        printf("Port \"%s\" could not be opened!\n", portname);
-        return sp_ret;
+      printf ( "Port \"%s\" could not be opened!\n", portname );
+      return sp_ret;
     }
 
-    /** Save current port configuration for later restoring */
-    sp_ret = sp_new_config(&savedPortConfig);
-    if (SP_OK > sp_ret)
+  /** Save current port configuration for later restoring */
+  sp_ret = sp_new_config ( &savedPortConfig );
+  if ( SP_OK > sp_ret )
     {
-        printf("Config of port \"%s\" could not be saved! (Out of memory?)\n",
-               portname);
-        return sp_ret;
+      printf ( "Config of port \"%s\" could not be saved! (Out of memory?)\n",
+               portname );
+      return sp_ret;
     }
-    sp_ret = sp_get_config(ctx.port, savedPortConfig);
-    if (SP_OK > sp_ret)
+  sp_ret = sp_get_config ( ctx.port, savedPortConfig );
+  if ( SP_OK > sp_ret )
     {
-        printf("Config of port \"%s\" could not be saved! (Read error?)\n",
-               portname);
-        return sp_ret;
-    }
-
-    sp_ret = sp_set_baudrate(ctx.port, SF_SERIAL_BAUDRATE);
-    if (SP_OK > sp_ret)
-    {
-        printf("Could not set baudrate to %u on port \"%s\"!\n",
-               SF_SERIAL_BAUDRATE, portname);
-        return sp_ret;
+      printf ( "Config of port \"%s\" could not be saved! (Read error?)\n",
+               portname );
+      return sp_ret;
     }
 
-    sp_ret = sp_set_bits(ctx.port, SF_SERIAL_BITS);
-    if (SP_OK > sp_ret)
+  sp_ret = sp_set_baudrate ( ctx.port, SF_SERIAL_BAUDRATE );
+  if ( SP_OK > sp_ret )
     {
-        printf("Could not set number of bits to %u on port \"%s\"!\n",
-               SF_SERIAL_BITS, portname);
-        return sp_ret;
+      printf ( "Could not set baudrate to %u on port \"%s\"!\n",
+               SF_SERIAL_BAUDRATE, portname );
+      return sp_ret;
     }
 
-    sp_ret = sp_set_parity(ctx.port, SP_PARITY_NONE);
-    if (SP_OK > sp_ret)
+  sp_ret = sp_set_bits ( ctx.port, SF_SERIAL_BITS );
+  if ( SP_OK > sp_ret )
     {
-        printf("Could not set parity to %u on port \"%s\"!\n",
-               SP_PARITY_NONE, portname);
-        return sp_ret;
+      printf ( "Could not set number of bits to %u on port \"%s\"!\n",
+               SF_SERIAL_BITS, portname );
+      return sp_ret;
     }
 
-    sp_ret = sp_set_stopbits(ctx.port, SF_SERIAL_STOPBITS);
-    if (SP_OK > sp_ret)
+  sp_ret = sp_set_parity ( ctx.port, SP_PARITY_NONE );
+  if ( SP_OK > sp_ret )
     {
-        printf("Could not set stop-bits to %u on port \"%s\"!\n",
-               SF_SERIAL_STOPBITS, portname);
-        return sp_ret;
+      printf ( "Could not set parity to %u on port \"%s\"!\n",
+               SP_PARITY_NONE, portname );
+      return sp_ret;
     }
 
-    sp_ret = sp_set_flowcontrol(ctx.port, SF_SERIAL_FLOWCTRL);
-    if (SP_OK > sp_ret)
+  sp_ret = sp_set_stopbits ( ctx.port, SF_SERIAL_STOPBITS );
+  if ( SP_OK > sp_ret )
     {
-        printf("Could not set flow-control to %u on port \"%s\"!\n",
-               SF_SERIAL_FLOWCTRL, portname);
-        return sp_ret;
+      printf ( "Could not set stop-bits to %u on port \"%s\"!\n",
+               SF_SERIAL_STOPBITS, portname );
+      return sp_ret;
     }
 
-    sf_serialmac_init(ctx.mac_ctx,
-                       (void *) ctx.port,
-                       (SF_SERIALMAC_HAL_READ_FUNCTION) sp_nonblocking_read, (SF_SERIALMAC_HAL_READ_WAIT_FUNCTION) sp_input_waiting,
-                       (SF_SERIALMAC_HAL_WRITE_FUNCTION) sp_nonblocking_write, read_evt, bufferRx_evt,
-                       write_evt, bufferTx_evt);
-
-    /** Start waiting for user input */
-    thread userInputEventLoop(wait4userinput);
-    userInputEventLoop.detach();
-
-    /* Loop until the user quits */
-    while (ctx.run)
+  sp_ret = sp_set_flowcontrol ( ctx.port, SF_SERIAL_FLOWCTRL );
+  if ( SP_OK > sp_ret )
     {
-        sleep(1);
-        sf_serialmac_entry((struct sf_serialmac_ctx *) ctx.mac_ctx);
+      printf ( "Could not set flow-control to %u on port \"%s\"!\n",
+               SF_SERIAL_FLOWCTRL, portname );
+      return sp_ret;
     }
 
-    if (NULL != ctx.port)
+  sf_serialmac_init ( ctx.mac_ctx,
+                      ( void * ) ctx.port,
+                      ( SF_SERIALMAC_HAL_READ_FUNCTION ) sp_nonblocking_read, ( SF_SERIALMAC_HAL_READ_WAIT_FUNCTION ) sp_input_waiting,
+                      ( SF_SERIALMAC_HAL_WRITE_FUNCTION ) sp_nonblocking_write, read_evt, bufferRx_evt,
+                      write_evt, bufferTx_evt );
+
+  /** Start waiting for user input */
+  thread userInputEventLoop ( wait4userinput );
+  userInputEventLoop.detach();
+
+  /* Loop until the user quits */
+  while ( ctx.run )
     {
-        /** Restore previous port configuration */
-        sp_ret = sp_set_config(ctx.port, savedPortConfig);
-        sp_free_port(ctx.port);
+      sleep ( 1 );
+      sf_serialmac_entry ( ( struct sf_serialmac_ctx * ) ctx.mac_ctx );
     }
-    return sp_ret;
+
+  if ( NULL != ctx.port )
+    {
+      /** Restore previous port configuration */
+      sp_ret = sp_set_config ( ctx.port, savedPortConfig );
+      sp_free_port ( ctx.port );
+    }
+  return sp_ret;
 }
-
