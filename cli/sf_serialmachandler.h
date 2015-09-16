6337f74e731dce712828f0b8bea19f4dfa4a866d
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-09-16 13:26:41 +0200

    CLI: Format lines to meet line length constraints

--- cli/sf_serialmachandler.h
+++ cli/sf_serialmachandler.h
@@ -17,7 +17,11 @@ public:
     static void Tx ( SerialMacCli* serialmaccli, char *buffer, size_t length );
 
 private:
-    static int InitSerialPort ( sp_port **port, const char *portname, sp_port_config **saved_port_config, sp_event_set **port_events );
+    static int InitSerialPort ( sp_port **port,
+                                const char *portname,
+                                sp_port_config **saved_port_config,
+                                sp_event_set **port_events
+                              );
     static void Wait4HalEvent ( sp_port *port,
                                 sp_event_set *port_events,
                                 enum sp_event event,
@@ -28,8 +32,14 @@ private:
                                 ( struct sf_serialmac_ctx* ctx ),
                                 int nanonap
                               );
-    static void ReadEvent ( void *mac_context, char *frame_buffer, size_t frame_buffer_length );
-    static void BufferRxEvent ( void *mac_context, char *frame_buffer, size_t frame_buffer_length );
+    static void ReadEvent ( void *mac_context,
+                            char *frame_buffer,
+                            size_t frame_buffer_length
+                          );
+    static void BufferRxEvent ( void *mac_context,
+                                char *frame_buffer,
+                                size_t frame_buffer_length
+                              );
     static void WriteEvent ( void *mac_context, size_t processed );
     static void BufferTxEvent ( void *mac_context, size_t processed );
     static bool filter ( Observer *observer, Event *event );
