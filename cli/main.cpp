b0158e009b296b13e9bced041d13c82badc5f737
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-10-05 15:01:14 +0200

    CLI: Add command line parameters to configure serial port
    fixes #3539

--- cli/main.cpp
+++ cli/main.cpp
@@ -12,114 +12,12 @@
  *
  =============================================================================*/
 
-#include <getopt.h>
-#include <cstddef>
-#include <string>
-#include <libgen.h>
-#include <iostream>
-
-#include "version.h"
 #include "sf_serialmaccli.h"
-
-static int usage ( int exitCode, char *commandPath );
-
-/* allowed short and long options */
-static const char *shortOptString = "p:idh";
-static const struct option longOptString[] =
-{
-  {"port", required_argument,      NULL, 'p'},
-  {"interactive", no_argument,      NULL, 'i'},
-  {"debug",    optional_argument,      NULL, 'd'},
-  {"help",    no_argument,            NULL, 'h'},
-  {0, 0, 0, 0}
-};
-
-static int usage ( int exitCode, char *commandPath )
-{
-  std::string companyName = SERIALMAC_PRODUCT_COMPANY;
-  std::string productName = SERIALMACCLI_PRODUCT_NAME;
-  std::string protocolName = SERIALMAC_PRODUCT_NAME;
-  char *commandName = basename ( commandPath );
-  std::ostream *out = &std::cout;
-
-  if ( 0 != exitCode )
-    {
-      out = &std::cerr;
-    }
-
-  *out << "Usage: "
-       << commandName
-       << " [OPTIONS]\n"
-       << companyName
-       << " "
-       << productName
-       << "\n"
-       << "Send and receive "
-       << companyName
-       << " "
-       << protocolName
-       << " frames via the serial interface.\n\n"
-       << "Default parameters:\n\n"
-       << "\tRunning "
-       << commandName
-       << " without parameters will use the following default values:\n\n"
-       << "\tserial port : The first available serial port.\n"
-       << "Optional arguments:\n\n"
-       << "\t-p\t\tSerial port.\n\n"
-       << "\t-d\t\tDebug information on stderr.\n"
-       << "\t-h\t\tThis helpful blurb.\n"
-       << "Exit status:\n\n"
-       << "\t0 if OK\n"
-       << "\t1 if error\n"
-       << std::endl;
-  exit ( exitCode );
-}
 
 int main ( int argc, char **argv )
 {
 
-  char *portname = NULL;
+  SerialMacCli cli;
 
-  int opt = 0;
-  int long_index = 0;
-
-  while ( ( opt = getopt_long ( argc, argv, shortOptString, longOptString, &long_index ) ) != -1 )
-    {
-      switch ( opt )
-        {
-        case 'p':
-          portname=optarg;
-          break;
-
-        case 'i':
-
-          break;
-
-        case 'd':
-          break;
-
-        case 'h':
-          usage ( 0, argv[0] );
-          break;
-        case '?':
-          usage ( 1, argv[0] );
-          break;
-
-        default:
-          /* You won't actually get here. */
-          std::cerr << "Error: This shouldn't have happened!" << std::endl;
-          break;
-        }
-    }
-
-  SerialMacCli cli ( portname );
-
-  if ( cli.Run() )
-    {
-      return 1;
-    }
-
-  while ( 1 );
-
-  return 0;
+  return cli.Run( argc, argv );
 }
