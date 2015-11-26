ef5c96e8994027b2a36dbffcf39fc70975ccaea7
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-11-26 16:17:21 +0100

    CLI: Enhance the API of HexStringToBinary and add more unit test cases
    refs #3540

--- cli/sf_stringhex.cpp
+++ cli/sf_stringhex.cpp
@@ -15,8 +15,8 @@ std::string *StringHex::BinaryToHexString ( uint8_t* binaries, size_t length )
   return NULL;
 }
 
-std::vector<uint8_t> &StringHex::HexStringToBinary ( std::string &hex_string )
+std::vector<uint8_t> &StringHex::HexStringToBinary ( std::string &hex_string,
+    std::vector<uint8_t> &hex_binaries )
 {
- std::vector<uint8_t> *hex = new std::vector<uint8_t>();
-  return *hex;
+  return hex_binaries;
 }
--- cli/test/sf_teststringhex.cpp
+++ cli/test/sf_teststringhex.cpp
@@ -30,10 +30,73 @@ void TestStringHex::TearDown()
 
 TEST_F ( TestStringHex, HexStringToBinaryTest )
 {
+  std::string hex_string = "55AA55FF00";
+  uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
+}
+
+TEST_F ( TestStringHex, LowerCaseHexStringToBinaryTest )
+{
+  std::string hex_string = "55aa55ff00";
+  uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
+}
+
+TEST_F ( TestStringHex, MixedCaseHexStringToBinaryTest )
+{
+  std::string hex_string = "55aA55Ff00";
+  uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
+}
+
+TEST_F ( TestStringHex, PrefixedHexStringToBinaryTest )
+{
+  std::string hex_string = "0x55AA55FF00";
+  uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
+}
+
+TEST_F ( TestStringHex, SpaceSeparatedHexStringToBinaryTest )
+{
+  std::string hex_string = "55 AA 55 FF 00";
+  uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
+}
+
+TEST_F ( TestStringHex, PrefixedSpaceSeparatedHexStringToBinaryTest )
+{
   std::string hex_string = "0x55 0xAA 0x55 0xFF 0x00";
   uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
   StringHex hex;
-  ASSERT_THAT ( hex.HexStringToBinary ( hex_string ), testing::ElementsAreArray ( hex_array_test ) ) << "eh?";
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
+}
+
+TEST_F ( TestStringHex, InvalidHexStringToBinaryTest )
+{
+  std::string hex_string = "INVALID";
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::IsEmpty() );
+}
+
+TEST_F ( TestStringHex, PartlyInvalidHexStringToBinaryTest )
+{
+  std::string hex_string = "55 AA 55 FF 00 INVALID";
+  uint8_t hex_array_test[] = { 0x55, 0xaa, 0x55, 0xff, 0x00 };
+  StringHex hex;
+  std::vector<uint8_t> hex_vector;
+  ASSERT_THAT ( hex.HexStringToBinary ( hex_string, hex_vector ), testing::ElementsAreArray ( hex_array_test ) );
 }
 
 TEST_F ( TestStringHex, BinaryToHexStringTest )
