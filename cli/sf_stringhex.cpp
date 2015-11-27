d7bb380fcbd448590d8203917a63739f1d226752
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-11-27 13:11:27 +0100

    CLI: Tokenize hex string at spaces
    Passes 8 out of 11 tests

--- cli/sf_stringhex.cpp
+++ cli/sf_stringhex.cpp
@@ -1,4 +1,5 @@
 #include "sf_stringhex.h"
+#include <string.h>
 
 StringHex::StringHex()
 {
@@ -19,27 +20,57 @@ std::vector<uint8_t> &StringHex::HexStringToBinary ( std::string &hex_string,
     std::vector<uint8_t> &hex_binaries )
 {
 
+  /** Needed for tokenizing string at spaces */
+  const char *delim = " ";
+  char *saveptr = NULL;
+  char *token = "  ";
+  char *c_hex_string;
+  char *str;
+
+  size_t hex_string_length;
+  size_t token_length;
   char hex_array[] = { 0, 0, 0};
-  unsigned long result;
+  unsigned long byte;
   char *endptr;
-  for ( size_t i = 0; i < hex_string.length(); i+=2 )
+
+  hex_string_length = hex_string.length();
+  /** Copy the whole string for tokenizing */
+  c_hex_string = ( char* ) std::malloc ( hex_string_length + 1 );
+  strcpy ( c_hex_string, hex_string.c_str() );
+  /* Copy the pointer so the tokenizer may set it to NULL */
+  str = c_hex_string;
+
+  do
     {
-      /* Check if there follows another character */
-      if ( hex_string.length() >= i + 1 )
+      token = strtok_r ( str, delim, &saveptr );
+      /** Set to NULL for all consecutive iterations */
+      str = NULL;
+      if ( token )
         {
-          * ( ( uint16_t* ) hex_array ) = * ( ( uint16_t* ) ( hex_string.c_str() + i ) );
-          result = strtoul ( hex_array, &endptr, 16 );
-          /* Check if the whole string had been valid */
-          if ( '\0' != *hex_array && '\0' == *endptr )
+          for ( size_t i = 0; i <
+                ( token_length = strnlen ( token, hex_string_length ) ); i+=2 )
             {
-              hex_binaries.push_back ( result );
-            }
-          else
-            {
-              break;
+              /* Check if there follows another character */
+              if ( token_length >= i + 1 )
+                {
+                  * ( ( uint16_t* ) hex_array ) =
+                    * ( ( uint16_t* ) ( token + i ) );
+                  byte = strtoul ( hex_array, &endptr, 16 );
+                  /* Check if the whole string had been valid */
+                  if ( '\0' != *hex_array && '\0' == *endptr )
+                    {
+                      hex_binaries.push_back ( byte );
+                    }
+                  else
+                    {
+                      /* Stop at the first invalid character */
+                      break;
+                    }
+                }
             }
         }
     }
+  while ( token );
 
   return hex_binaries;
 }
