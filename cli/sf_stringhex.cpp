22c134867173d82ecb457820019b5049f96684c1
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-11-27 11:46:23 +0100

    CLI: Add simple string to binary conversion which passes 4 out of 10 tests
    refs #3540

--- cli/sf_stringhex.cpp
+++ cli/sf_stringhex.cpp
@@ -18,5 +18,17 @@ std::string *StringHex::BinaryToHexString ( uint8_t* binaries, size_t length )
 std::vector<uint8_t> &StringHex::HexStringToBinary ( std::string &hex_string,
     std::vector<uint8_t> &hex_binaries )
 {
+
+  char hex_array[] = { 0, 0, 0};
+  for ( size_t i = 0; i < hex_string.length(); i+=2 )
+    {
+      /* Check if there follows another character */
+      if ( hex_string.length() >= i + 1 )
+        {
+          * ( ( uint16_t* ) hex_array ) = * ( ( uint16_t* ) ( hex_string.c_str() + i ) );
+          hex_binaries.push_back ( strtoul ( hex_array,NULL,16 ) );
+        }
+    }
+
   return hex_binaries;
 }
