973932a4c44230ad4b7c8f9b5272572fe7a52a5d
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-11-27 11:47:33 +0100

    CLI: Add error handling to string to binary conversion
    Passes 5 out of 10 tests
    
    refs #3540

--- cli/sf_stringhex.cpp
+++ cli/sf_stringhex.cpp
@@ -20,13 +20,20 @@ std::vector<uint8_t> &StringHex::HexStringToBinary ( std::string &hex_string,
 {
 
   char hex_array[] = { 0, 0, 0};
+  unsigned long result;
+  char *endptr;
   for ( size_t i = 0; i < hex_string.length(); i+=2 )
     {
       /* Check if there follows another character */
       if ( hex_string.length() >= i + 1 )
         {
           * ( ( uint16_t* ) hex_array ) = * ( ( uint16_t* ) ( hex_string.c_str() + i ) );
-          hex_binaries.push_back ( strtoul ( hex_array,NULL,16 ) );
+          result = strtoul ( hex_array, &endptr, 16 );
+          /* Check if the whole string had been valid */
+          if ( '\0' != *hex_array && '\0' == *endptr )
+            {
+              hex_binaries.push_back ( result );
+            }
         }
     }
 
