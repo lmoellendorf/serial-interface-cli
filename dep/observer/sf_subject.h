fb80fef783b427b2ab864fb386190912bfed6790
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-12-21 14:58:26 +0100

    Apply auto-formatting

--- dep/observer/sf_subject.h
+++ dep/observer/sf_subject.h
@@ -15,12 +15,12 @@ public:
     Subject();
     ~Subject();
 
-    typedef bool (*Filter) ( Observer *observer, Event *event );
+    typedef bool ( *Filter ) ( Observer *observer, Event *event );
     //TODO: overload Attach() to enable the Oberserver to pass a Event "id"
     // it is interested in
     static void Attach ( Observer *observer );
     static void Detach ( Observer *observer );
-    static void Notify ( Event *event, Filter filter);
+    static void Notify ( Event *event, Filter filter );
 };
 
 #endif // SUBJECT_H
