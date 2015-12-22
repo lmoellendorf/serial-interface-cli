62df879457f509891e6a9dcb8c145310814d3f92
Lars Moellendorf <lars.moellendorf@stackforce.de>
2015-12-22 14:35:50 +0100

    Add file comments to observer module
    fixes #4217

--- dep/observer/sf_event.cpp
+++ dep/observer/sf_event.cpp
@@ -1,3 +1,19 @@
+/**
+ * @code
+ *  ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ * / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ * \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ * |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ * embedded.connectivity.solutions.==============
+ * @endcode
+ *
+ * @file
+ * @copyright  STACKFORCE GmbH, Heitersheim, Germany, http://www.stackforce.de
+ * @author     STACKFORCE
+ * @brief      Generic implementation of the Oberserver pattern.
+ *
+ */
+
 #include "sf_event.h"
 
 namespace sf
--- dep/observer/sf_observer.h
+++ dep/observer/sf_observer.h
@@ -1,6 +1,67 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+/**
+ * @code
+ *  ___ _____ _   ___ _  _____ ___  ___  ___ ___
+ * / __|_   _/_\ / __| |/ / __/ _ \| _ \/ __| __|
+ * \__ \ | |/ _ \ (__| ' <| _| (_) |   / (__| _|
+ * |___/ |_/_/ \_\___|_|\_\_| \___/|_|_\\___|___|
+ * embedded.connectivity.solutions.==============
+ * @endcode
+ *
+ * @file
+ * @copyright  STACKFORCE GmbH, Heitersheim, Germany, http://www.stackforce.de
+ * @author     STACKFORCE
+ * @brief      Generic implementation of the Oberserver pattern.
+ *
+ * @details    C++ does not allow C++ Member functions as C-Callbacks.
+ * However, callback functions are needed by interrupt handlers or
+ * event handlers. The problem can be solved by applying the Observer pattern:
+ *
+ *
+ * The "Subject" passes pointers to its static methods as callback functions.
+ * Whenever one of those callbacks are called, the "Subject" notifies all
+ * attached "Observers".
+ *
+ * @code
+ *                                  +-----------------------------------------+
+ *                                  |  Subject                                |
+ * +--------------------------+     |                                         |
+ * |                          |     +-----------------------------------------+
+ * |  Observer                +---<>|                                         |
+ * |                          |     | +ObserverCollection                     |
+ * +--------------------------+     |                                         |
+ * |                          |     +-----------------------------------------+
+ * +--------------------------+     |                                         |
+ * |                          |     | +Attach ( Observer *observer )          |
+ * | +Update ( Event *event ) |     |                                         |
+ * |                          |     | +Detach ( Observer *observer )          |
+ * +--------------------------+     |                                         |
+ *                        /_\       | +Notify ( Event *event, Filter filter ) |
+ *                         |        |                                         |
+ *                         |        +-----------------------------------------+
+ *                         |
+ *             +-----------+---------------------+
+ *             |                                 |
+ *             |                                 |
+ *             |                                 |
+ * +-----------+--------------+     +------------+-------------+
+ * |                          |     |                          |
+ * |  ConcreteObserverA       |     |  ConcreteObserverB       |
+ * |                          |     |                          |
+ * +--------------------------+     +--------------------------+
+ * |                          |     |                          |
+ * +--------------------------+     +--------------------------+
+ * |                          |     |                          |
+ * | +Update ( Event *event ) |     | +Update ( Event *event ) |
+ * |                          |     |                          |
+ * +--------------------------+     +--------------------------+
+ * @endcode
+ *
+ * This is a generic C++ implementation of this pattern.
+ */
+
 #include "sf_event.h"
 
 namespace sf
