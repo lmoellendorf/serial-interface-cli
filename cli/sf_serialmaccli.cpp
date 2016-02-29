lmoellendorf/serial-interface-cli

commit ddbffff00a70cc06ceeb6534efc4dfda6a740a83

Add more error handling
refs #4128

cli/sf_serialmaccli.cpp
@@ -636,13 +636,20 @@ void SerialMacCli::CliInput ( void )
            * cli_input_state to CLI.
            */
           sf_serialmac_hal_tx_callback ( mac_context );
-          if ( SP_OK != sp_wait ( port_tx_event, 0 ) )
+          if ( SF_SERIALMAC_SUCCESS !=
+               sf_serialmac_hal_tx_callback ( mac_context ) ||
+               SP_OK != sp_wait ( port_tx_event, 0 ) )
             {
               std::cerr << "Error during transmission on \"" << port_name
-              << "\"!"<< std::endl;
+                        << "\"!"<< std::endl;
+              cli_input_state = QUIT;
             }
           break;
 
+        default:
+          std::cerr << "Error during transmission on \"" << port_name
+                    << "\"!"<< std::endl;
+          // break omitted
         case QUIT:
           //TODO: use other means for quitting
           Verbose ( "Quitting.\n" );
@@ -679,6 +686,10 @@ void SerialMacCli::CliOutput ( void )
         case CLI:
           // currently not used
           break;
+        default:
+          std::cerr << "Error during reception on \"" << port_name
+                    << "\"!"<< std::endl;
+          // break omitted
         case QUIT:
           Verbose( "Quitting.\n" );
           /** This stops the other thread. */

commit c461e637ecea4cc7129c5b237782c29280089b20

Make error handling work on Windows
Fix regression introduced in commit:ddbffff0

refs #4128

cli/sf_serialmaccli.cpp
@@ -562,6 +562,7 @@ void SerialMacCli::CliInput ( void )
   std::string delimiters;
   char *output_buffer = NULL;
   int output_buffer_length = 0;
+  sf_serialmac_return ret;
 
   /** Repeat until the user stops you */
   while ( run )
@@ -653,9 +654,8 @@ void SerialMacCli::CliInput ( void )
            * Call the callback to process transmission until Update() sets
            * cli_input_state to CLI.
            */
-          sf_serialmac_hal_tx_callback ( mac_context );
-          if ( SF_SERIALMAC_SUCCESS !=
-               sf_serialmac_hal_tx_callback ( mac_context ) ||
+          ret = sf_serialmac_hal_tx_callback ( mac_context );
+          if ( (SF_SERIALMAC_SUCCESS != ret && SF_SERIALMAC_ERROR_HAL_BUSY != ret)||
                SP_OK != sp_wait ( port_tx_event, 0 ) )
             {
               std::cerr << "Error during transmission on \"" << port_name